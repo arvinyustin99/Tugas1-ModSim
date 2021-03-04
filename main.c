6642c26b522db1aa638ca970fda40790f9703638
arvinyustin99 <arvinyustin99@gmail.com>
2021-03-04 21:08:05 +0700

    add looping body

--- main.c
+++ main.c
@@ -22,12 +22,13 @@
 #define NUM_STATIONS 3
 
 int num_stations,
+    current_station,
     route[NUM_STATIONS + 1];
 double  mean_interarrival[NUM_STATIONS + 1],
         length_simulation,
         prob_stations_1_2[2 + 1];
 
-void arrive(){
+void arrive(int current_gate){
     event_schedule(sim_time + expon(mean_interarrival[1]), EVENT_ADD_QUEUE_1);
     event_schedule(sim_time + expon(mean_interarrival[2]), EVENT_ADD_QUEUE_2);
     event_schedule(sim_time + expon(mean_interarrival[3]), EVENT_ADD_QUEUE_3);
@@ -36,9 +37,25 @@ void arrive(){
 
 int main(){
   init_simlib();
+
+
   event_schedule(length_simulation, EVENT_END_SIMUL);
   do{
     timing();
+
+    switch(next_event_type){
+      case EVENT_QUEUE_1:
+      case EVENT_QUEUE_2:
+      case EVENT_QUEUE_3:
+        arrive(next_event_type);
+        break;
+      case EVENT_LOAD:
+        load(current_station);
+        break;
+      case EVENT_UNLOAD:
+        unload(current_station);
+        break;
+    }
   }while (next_event_type != EVENT_END_SIMUL);
 }
 
